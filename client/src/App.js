3c37fa1ce73b7610c5d63fe94dbc4ff829873f62
Raiane Silva <raianesirlene9@gmail.com>
2024-03-26 20:31:11 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>This is Home!</div>,
+    },
+  ]);
+
+function App(){
+    return (
+        <div>
+            <RouterProvider router={router}/>
+        </div>
+    );
+}
+
+export default App;
